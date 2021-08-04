2a3577acf3ff10b1fb496bc8d2e0ceaf1a28222e
UnTea <weqqu2014@yandex.ru>
2021-08-05 01:48:40 +0300

    add reader library

--- reader/reader.cpp
+++ reader/reader.cpp
@@ -1,5 +1,15 @@
-//
-// Created by UnT3a on 04.08.2021.
-//
-
 #include "reader.hpp"
+
+
+std::string Reader::read_string_to_line_break() {
+    std::string string;
+
+    while (index < data.size() && data[index] != '\n') {
+        string += data[index];
+        index++;
+    }
+
+    index++;
+
+    return string;
+}
--- reader/reader.hpp
+++ reader/reader.hpp
@@ -1,1 +1,22 @@
-#include 
+#pragma once
+
+
+#include <string>
+#include <vector>
+#include <cstdint>
+
+
+#include "hdr.hpp"
+
+
+struct Reader {
+    std::vector<uint8_t>& data;
+    size_t index;
+
+    explicit Reader(std::vector<uint8_t>& file)
+    : data(file)
+    , index()
+    {}
+
+    std::string read_string_to_line_break();
+};
